1d795af3af98df749e925621188a19ed295a2c35
Atanas Kolev <naskokolev00@gmail.com>
2019-08-13 14:44:40 +0300

    8 queens problem - not finished

---
+++ EightQueenProblem.cs
@@ -0,0 +1,145 @@
+﻿namespace Algorithms_and_data_structure
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class EightQueenProblem
+    {
+        private int queenMark = 1;
+        private int queenFieldsMark = 2;
+        private int defaultBoardValue = 0;
+
+        int[,] board;
+
+        public EightQueenProblem()
+        {
+            board = new int[,] {
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+        }
+
+        public void Solve(int row)
+        {
+            if (row == 8)
+            {
+                int rowLength = this.board.GetLength(0);
+                int colLength = this.board.GetLength(1);
+
+                for (int i = 0; i < rowLength; i++)
+                {
+                    for (int j = 0; j < colLength; j++)
+                    {
+                        Console.Write(string.Format("{0} ", this.board[i, j]));
+                    }
+
+                    Console.Write(Environment.NewLine + Environment.NewLine);
+                }
+
+                Console.ReadLine();
+            }
+
+            else
+            {
+                for (int col = 0; col <= 7; col++)
+                {
+                    if (CanPlaceQueen(row, col))
+                    {
+                        MarkAttackedPositions(row, col);
+                        Solve(row + 1);
+                        UnMarkAttackedPositions(row, col);
+                    }
+                }
+            }
+        }
+
+        private bool CanPlaceQueen(int row, int col)
+        {
+            if (this.board[row, col] == 0)
+            {
+                this.board[row, col] = queenMark;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void MarkAttackedPositions(int givenRow, int givenCol)
+        {
+            int givenRowCopy = givenRow;
+            int givenColCopy = givenCol;
+
+            for (int col = givenCol + 1; col < this.board.GetLength(1); col++) // right
+            {
+                this.board[givenRow, col] = queenFieldsMark;
+            }
+
+            for (int row = givenRow + 1; row < this.board.GetLength(0); row++) // down
+            {
+                this.board[row, givenCol] = queenFieldsMark;
+            }
+
+            for (int col = givenCol - 1; col >= 0; col--) // left
+            {
+                this.board[givenRow, col] = queenFieldsMark;
+            }
+
+            for (int row = givenRow - 1 ; row >= 0; row--) // up
+            {
+                this.board[row, givenCol] = queenFieldsMark;
+            }
+
+            while (givenRowCopy < 7 && givenColCopy < 7) // bootom right diagonal
+            {
+                givenRowCopy++;
+                givenColCopy++;
+
+                this.board[givenRowCopy, givenColCopy] = queenFieldsMark;
+            }
+
+            givenRowCopy = givenRow;
+            givenColCopy = givenCol;
+
+            while (givenRowCopy < 7 && givenColCopy > 0) // bootom left diagonal
+            {
+                givenRowCopy++;
+                givenColCopy--;
+
+                this.board[givenRowCopy, givenColCopy] = queenFieldsMark;
+            }
+
+            givenRowCopy = givenRow;
+            givenColCopy = givenCol;
+
+            while (givenRowCopy > 0 && givenColCopy > 0) // top left diagonal
+            {
+                givenRowCopy--;
+                givenColCopy--;
+
+                this.board[givenRowCopy, givenColCopy] = queenFieldsMark;
+            }
+
+            givenRowCopy = givenRow;
+            givenColCopy = givenCol;
+
+            while (givenRowCopy > 0 && givenColCopy < 7) // top right diagonal
+            {
+                givenRowCopy--;
+                givenColCopy++;
+
+                this.board[givenRowCopy, givenColCopy] = queenFieldsMark;
+            }
+        }
+
+        private void UnMarkAttackedPositions(int givenRow, int givenCol)
+        {
+           //
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,38 +4,8 @@
     {
         public static void Main()
         {
-            // Sum numbers recursivly
-            var sumNumbers = new RecursionSumNumbers();
-            var sum = sumNumbers.Sum(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 0);
-
-            // Factorial with recursion
-            var factorial = new FactorialRecursion();
-            var result = factorial.Factorial(5);
-
-
-            // Draw on console with recursioon
-            var drawObj = new DrawRecursion();
-            //drawObj.Print(5);
-
-            // Generating 0/1 Vectors
-            var vectorGenerator = new VectorGenerator();
-            //vectorGenerator.Generate(new int[3], 0);
-
-            // Generating combinations
-            var combinationsGenerator = new CombinationsGenerator();
-            //combinationsGenerator.Generate(new int[] { 1, 2, 3, 4, 5 }, new int[3], 0, -1);
-
-
-            // Sudoku 3x3 basic demo
-            var sudoku = new Sudoku();
-
-            // just making some example to see if it actually seek for the solution, can change the numbers however i like. It works only with numbers to 3...Becouse it is a simple demo :D
-            int[][] threeByThreeSudoku = new int[3][];
-            threeByThreeSudoku[0] = new int[3] { 0, 0, 1 };
-            threeByThreeSudoku[1] = new int[3] { 0, 0, 0 };
-            threeByThreeSudoku[2] = new int[3] { 0, 0, 0 };
-
-            sudoku.Solve(threeByThreeSudoku, 0,0);
+            EightQueenProblem problem = new EightQueenProblem();
+            problem.Solve(0);
         }
     }
-}
+}       
