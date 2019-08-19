74992370ab062e76cd3cb5ce6847f4c3d230bc7c
Atanas Kolev <naskokolev00@gmail.com>
2019-08-19 12:50:49 +0300

    added combination algorithm - not finished

---
+++ CombinationAlgorithms/Combination.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms_and_data_structure.CombinationAlgorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Combination
+    {
+        private int k;
+
+        public Combination(int k)
+        {
+            this.k = k;
+        }
+
+        public void Combine(int index, int start, string[] array)
+        {
+            if (index >= k)
+            {
+                Console.WriteLine(string.Join(" ", array));
+            }
+
+            else
+            {
+                for (int i = start; i < array.Length; i++)
+                {
+                    //todo
+                }
+            }
+        }
+    }
+}
